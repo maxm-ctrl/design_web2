12fe7f784f2420b1442e634f150365ef1b5d441e
Ângelo Max Ananias de Moura <max.m@escolar.ifrn.edu.br>
2026-08-20 13:11:01 -0300

    jss

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
 alert("olá mundo!");
+
+var nasc = 1991;       
+let nome = "Pedrina";   
+const viva = true;      
+let altura = 1.64;      
+
+
+if (viva) {
+
+    let saudacao = "Olá, " + nome + "!";
+  
+    let mensagem = `Altura: ${altura}m | Idade: ${2025-nasc}`;
+    
+    alert(saudacao+'\n'+mensagem);
+} else {
+  
+    alert("Você não está viva!");
+}
