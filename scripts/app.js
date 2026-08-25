8db833d06f24d3353a1be55df7925d3a1043b548
Ângelo Max Ananias de Moura <max.m@escolar.ifrn.edu.br>
2026-08-25 15:42:52 -0300

    ai ai

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,13 @@
 var nasc = 1991;
-let nome  = "pedrina";
+let nome = "pedrina";
 const viva = true;
 
-function calcIdade(ano=2026){
-    let idade  = ano - nasc;
-    alert (`dentro de função - idade ${idade}`);
+function calcIdade(ano = 2026){
+    let idade = ano - nasc;
+    alert(`dentro de função - idade ${idade}`);
     return idade;
 }
 
+let idade = calcIdade();
 
-alert (`fora da funcao  :  idade ${idade}`);
-alert(`fora da funcao  : chamando calcIdade ${calcIdade()}`)
+alert(`fora da função: idade ${idade}`);
