ec29e26232f5c844306733d9a14cbfae53e05b9d
Ângelo Max Ananias de Moura <max.m@escolar.ifrn.edu.br>
2026-08-26 17:48:16 -0300

    lol123

---
+++ js/app3.js
@@ -0,0 +1,15 @@
+let nasc = 2009;
+let nome = "Ângelo";
+let vivo = true;
+let altura = 1.75;
+let sexo = "Masculino";
+let peso = null;
+let imc = "peso"*10
+
+alert (`Ano de nascimento - tipo: ${typeof nasc} - valor: ${nasc}`)
+alert(`Nome - tipo: ${typeof nome} - valor: ${nome}`)
+alert(`Vivo - tipo: ${typeof vivo} - valor: ${vivo}`)
+alert(`Altura - tipo: ${typeof altura} - valor: ${altura}`)
+alert(`Sexo - tipo: ${typeof sexo} - valor: ${sexo}`)
+alert(`Peso - tipo: ${typeof peso} - valor: ${peso}`)
+alert(`IMC - tipo: ${typeof imc} - valor: ${imc}`)
