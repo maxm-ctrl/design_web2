09a9272d8e06ca596c5385e53ea5b32a60a69b1c
Ângelo Max Ananias de Moura <max.m@escolar.ifrn.edu.br>
2026-08-26 17:29:14 -0300

    correção

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,29 @@
-var nasc = 2009
+var nasc = 2009;
+
 let nome = "Ângelo";
+
 const viva = true;
 
-function calcIdade(ano = 2026){
+function calcIdade(ano = 2026) {
+
     let idade = ano - nasc;
-    if (idade<18){
-        alert(`${nome} é  menor de idade.`)
+    let menor;
+    let podebeber;
+
+    if (idade < 18) {
+        menor = true;
+        podebeber = false;
+
+        alert(`${nome} é menor de idade.`);
+    } else {
+        menor = false;
+        podebeber = true;
     }
-    if (idade < 18){
-        let menor = true;
-        var podebeber = false;
-    }
-    else{
-        let menor = false;
-        var podebeber = true;
-    }
-    alert(`${nome} é menor de idade? ${menor}\nIdade: ${idade}\n Pode beber? ${podebeber}`);
+
+    alert(`${nome} é menor de idade? ${menor}
+Idade: ${idade}
+Pode beber? ${podebeber}`);
+
     return idade;
 }
 
