361890126f960a952c2cdba5a58d1c46f4b4411a
Ângelo Max Ananias de Moura <max.m@escolar.ifrn.edu.br>
2026-08-28 14:01:33 -0300

    operadores

---
+++ js/app2.js
@@ -0,0 +1,11 @@
+let n1 = prompt("Digite o primeiro número: ");
+n1 = Number(n1);
+let n2 = prompt("Digite o segundo número: ");
+n2 = Number(n2);
+// operações aritméticas
+alert(`${n1} + ${n2} = ${n1+n2}`);
+alert(`${n1} - ${n2} = ${n1-n2}`);
+alert(`${n1} * ${n2} = ${n1*n2}`);
+alert(`${n1} / ${n2} = ${n1/n2}`);
+alert(`${n1} % ${n2} = ${n1%n2}`);
+alert(`${n1} ** ${n2} = ${n1**n2}`);
