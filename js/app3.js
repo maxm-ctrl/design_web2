350ffa2e747e254b861c071a377ff8ee50526fa2
Ângelo Max Ananias de Moura <max.m@escolar.ifrn.edu.br>
2026-08-28 14:12:50 -0300

    comparação

---
+++ js/app3.js
@@ -0,0 +1,12 @@
+let n1 = prompt("Digite o primeiro número: ");
+n1 = Number(n1);
+let n2 = prompt("Digite o segundo número: ");
+n2 = Number(n2);
+alert (`${n1} == ${n2} = ${n1==n2}`);
+alert (`${n1} === ${n2} = ${n1===n2}`);
+alert (`${n1} > ${n2} = ${n1>n2}`);
+alert (`${n1} >= ${n2} = ${n1>=n2}`);
+alert (`${n1} < ${n2} = ${n1<n2}`);
+alert (`${n1} <= ${n2} = ${n1<=n2}`);
+alert (`${n1} != ${n2} = ${n1!=n2}`);
+alert (`${n1} !== ${n2} = ${n1!==n2}`);
