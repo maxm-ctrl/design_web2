1225e859a73c97a2e5b7ec1995bbdd68b7ba0579
Ângelo Max Ananias de Moura <max.m@escolar.ifrn.edu.br>
2026-09-04 13:47:58 -0300

    sim

--- js/app.js
+++
@@ -1,9 +0,0 @@
-let num = Number(prompt("Digite um número: "));
-
-if (num % 2 != 0){
-    alert(`${num} é ímpar`);
-}else if(num == 0){
-    alert(`${num} é ZERO!`);
-}else{
-    alert(`${num} é par`);
-}
